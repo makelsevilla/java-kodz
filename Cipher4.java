01365941fff75094a4d8546f521418fbe32c826e
Michael Sevilla <makelsevilla@gmail.com>
2024-06-14 19:48:08 +0800

    updated Cipher4 class. added throw exception line at encrypt method.

--- Cipher4.java
+++ Cipher4.java
@@ -5,41 +5,52 @@ public class Cipher4 {
         String baseKey = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ\s";
         String encryptionKey = "IBdnlbfPsLWYXqENRmyvVoHJTCDupAcFjOhxwrUazZSiMQGtgekK\n";
 
-        // convertBaseKeyStringToHashMap test
-        System.out.println(convertKeyStringToHashMap("aabbcC"));
+        try {
 
-        // encrypt test
-        String encryptedText = encrypt("Ang ganda ganda mo", baseKey, encryptionKey);
-        System.out.println(encryptedText);
+            // convertBaseKeyStringToHashMap test
+            System.out.println(convertKeyStringToHashMap("aabbcC"));
 
-        //decrypt test
-        System.out.println(decrypt(encryptedText, baseKey, encryptionKey));
+            // encrypt test
+            String encryptedText = encrypt("Ang ganda ganda mo", baseKey, encryptionKey);
+            System.out.println(encryptedText);
+
+            // decrypt test
+            System.out.println(decrypt(encryptedText, baseKey, encryptionKey));
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
-    private static String encrypt(String input, String baseKey, String encryptionKey) {
+    private static String encrypt(String input, String baseKey, String encryptionKey) throws Exception {
         // baseKey and encryptionKey should be equal in length, if not throw an error
+        if (baseKey.length() != encryptionKey.length()) {
+            throw new Exception(
+                    "The keys provided is not equal in length. Please make sure that the provided keys are correct");
+        }
 
         StringBuilder encryptedInputBuilder = new StringBuilder();
 
         HashMap<Character, Integer> baseKeyHashMap = convertKeyStringToHashMap(baseKey);
-        for(char ch: input.toCharArray()) {
+        for (char ch : input.toCharArray()) {
             char inclusionChar;
 
             // encrypts the character
-            if(baseKeyHashMap.get(ch) != null) {
+            if (baseKeyHashMap.get(ch) != null) {
                 int currCharBaseKeyIndex = baseKeyHashMap.get(ch);
                 inclusionChar = encryptionKey.charAt(currCharBaseKeyIndex);
             } else {
                 inclusionChar = ch;
             }
-            
+
             encryptedInputBuilder.append(inclusionChar);
         }
 
         return encryptedInputBuilder.toString();
     }
 
-    private static String decrypt(String input, String baseKey, String encryptionKey) {
+    private static String decrypt(String input, String baseKey, String encryptionKey) throws Exception {
 
         return encrypt(input, encryptionKey, baseKey);
     }
@@ -49,11 +60,11 @@ public class Cipher4 {
         HashMap<Character, Integer> keyIndexMap = new HashMap<>();
 
         int currKeyIndexMap = 0;
-        for(int currIdx = 0; currIdx < key.length(); currIdx++) {
+        for (int currIdx = 0; currIdx < key.length(); currIdx++) {
             char currChar = key.charAt(currIdx);
 
             // ignore duplicate characters in baseKey
-            if(! keyIndexMap.containsKey(currChar)) {
+            if (!keyIndexMap.containsKey(currChar)) {
                 keyIndexMap.put(currChar, currKeyIndexMap);
                 currKeyIndexMap++;
             }
